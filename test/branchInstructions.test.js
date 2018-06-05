e6857d94c7e55fe3bd905fa11048d557012854da
sondre <slaich87@gmail.com>
2018-06-05 23:42:31 +0200

    cond call test

--- test/branchInstructions.test.js
+++ test/branchInstructions.test.js
@@ -50,9 +50,9 @@ test('condcall', () => {
   state.registers.sp = 256;
   state.memory[state.registers.pc + 1] = 12;
   state.memory[state.registers.pc + 2] = 0;
-  state.conditionFlags.z = 1;
+  state.conditionFlags.cy = 1;
   const newState = copyState(state);
-  condCall(newState, 0b001); // pattern for z == 1
+  condCall(newState, 0b011); // pattern for cy == 1
   expect(newState.memory[state.registers.sp - 1]).toBe(0);
   expect(newState.memory[state.registers.sp - 2]).toBe(128);
   expect(newState.registers.sp).toBe(state.registers.sp - 2);
@@ -65,8 +65,8 @@ test('condcall', () => {
   state.registers.sp = 256;
   state.memory[state.registers.pc + 1] = 12;
   state.memory[state.registers.pc + 2] = 0;
-  state.conditionFlags.z = 1;
+  state.conditionFlags.cy = 1;
   const newState = copyState(state);
-  condCall(newState, 0b000); // pattern for z == 0
+  condCall(newState, 0b010); // pattern for cy == 0
   expect(newState.registers.pc).toBe(129);
 });
